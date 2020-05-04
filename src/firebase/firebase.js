cc0682c7f99dc2f0b365429ad1527e07c4d96bc3
Nitin <rames11a@uwindsor.ca>
2020-05-03 19:45:39 -0700

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,14 +3,7 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCoF9_EHc17iiP4BbF9Eb6M9mJaBHbtEG0",
-    authDomain: "pizzaro-staging.firebaseapp.com",
-    databaseURL: "https://pizzaro-staging.firebaseio.com",
-    projectId: "pizzaro-staging",
-    storageBucket: "pizzaro-staging.appspot.com",
-    messagingSenderId: "517159839848",
-    appId: "1:517159839848:web:a7208f0ebebefc9717b3dd",
-    measurementId: "G-HD6MZV0280"
+ //api cedentials here
 };
 
 export const myFirebase = firebase.initializeApp(firebaseConfig);
