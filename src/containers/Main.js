972e449bb7987b28a72db9887e77338c1379dd89
NitinR99 <rames11a@uwindsor.ca>
2020-05-04 13:51:43 -0700

    removed redux, secure routing, bootstrap and added functional components and materials ui

---
+++ src/containers/Main.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import fire from "../firebase";
+import history from "./History";
+import Login from "./Login";
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import { LinkContainer } from "react-router-bootstrap";
+import Routes from "../Routes";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+AppBar: {
+    backgroundColor:"purple",
+},
+}));
+
+export default function ButtonAppBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" className={classes.AppBar}>
+        <Toolbar>
+	<LinkContainer to="/">
+          <Typography variant="h4" className={classes.title}>
+            Pizzaroo
+          </Typography>
+	</LinkContainer>
+          
+        <Button variant="contained" color="primary" href="/login">
+	<Typography variant="h6">
+      Login
+	</Typography>
+    </Button>
+
+        </Toolbar>
+      </AppBar>
+<Routes />
+    </div>
+  );
+}
+/**
+import React from 'react';
+import { LinkContainer } from "react-router-bootstrap";
+import { Link } from "react-router-dom";
+import Routes from "../Routes";
+
+import Button from '@material-ui/core/Button';
+
+
+export default function main() {
+  
+
+  return (
+    <div>
+          <LinkContainer to="/login">
+        <Button variant="contained" color="primary">
+      Login
+    </Button>
+</LinkContainer>
+<Routes />
+</div>
+  );
+}
+**/
---
+++ src/containers/style.js
@@ -0,0 +1,34 @@
+const React = require("react");
+export default {
+  logoText: {
+    fontSize: 40,
+    fontWeight: "800",
+    marginTop: 150,
+    marginBottom: 30,
+    textAlign: "center",
+  },
+  loginFormTextInput: {
+    height: 43,
+    fontSize: 14,
+    borderRadius: 20,
+    borderWidth: 1,
+    borderColor: "purple",
+    backgroundColor: "#fafafa",
+     
+    marginTop: 20,
+    marginBottom: 20,
+    width:"100%",
+  },
+  loginButton: {
+    color: "white",
+    backgroundColor: "purple",
+    borderRadius: 25,
+    height: 45,
+    width: 95,
+    marginTop: 20,
+    
+  },
+  errorInput: {
+    textAlign: "center",
+  },
+};
