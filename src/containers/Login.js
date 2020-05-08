477febb5ed84a4b3a2bf26a57a3b1ffa18048998
Charles <cdcorro@hotmail.ca>
2020-05-07 20:19:49 -0400

    cleaned

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -7,88 +7,99 @@ import Container from '@material-ui/core/Container';
 
 
 class Login extends Component {
-	constructor(props) {
-		super(props);
-		this.login = this.login.bind(this);
-		this.handleChange = this.handleChange.bind(this);
-		this.state = {
-			email: "",
-			password: "",
-		};
-	}
-
-	handleChange(e) {
-		this.setState({ [e.target.name]: e.target.value });
-	}
-
-	login(e) {
-		e.preventDefault();
-		fire
-			.auth()
-			.signInWithEmailAndPassword(this.state.email, this.state.password)
-			.then((u) => {})
-			.catch(
-				(error) => {
-					console.log(error);
-				}
-			);
-	}
 	
-	render() {
-		return (
-			<div className="Login">
-				<form>
-					<h1 style = {styles.logoText}>
-						Welcome
-					</h1>
-					<Container maxWidth = "sm">
-						<div className = "form-group">
-							<input
-							style = {styles.loginFormTextInput}
-							value = {this.state.email}
-							onChange = {this.handleChange}
-							type = "email"
-							name = "email"
-							class = "form-control"
-							id = "exampleInputEmail1"
-							aria-describedby="emailHelp"
-							placeholder="username"
-							/>
-						</div>
-			
-						<div>
-							<input
-							style={styles.loginFormTextInput}
-							value={this.state.password}
-							onChange={this.handleChange}
-							type="password"
-							name="password"
-							class="form-control"
-							id="exampleInputPassword1"
-							placeholder="Password"
-							/>
-						</div>
-						
-						<div style={{textAlign:"center"}}>
-							<button
-							style={styles.loginButton}
-							type="submit"
-							onClick={
-								(e) => {
-									this.login(e);
-								}
-							}
-							class="btn btn-primary"
-							>
-								Login
-							</button>
-						</div>
-						
-					</Container>
-				</form>
-			</div>
-		);
-	}
+  constructor(props) {
+	  
+    super(props);
+    this.login = this.login.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      email: "",
+      password: "",
+    };
+	
+  }
+
+  handleChange(e) {
+	  
+    this.setState({ [e.target.name]: e.target.value });
+	
+  }
+
+  login(e) {
+	  
+    e.preventDefault();
+	
+    fire
+      .auth()
+      .signInWithEmailAndPassword(this.state.email, this.state.password)
+      .then((u) => {})
+      .catch(
+        (error) => {
+          console.log(error);
+        }
+      );
+	  
+  }
+  
+  render() {
+    return (
+	
+      <div className="Login">
+        <form>
+		
+          <h1 style = {styles.logoText}>
+            Welcome
+          </h1>
+		  
+          <Container maxWidth = "sm">
+            <div className = "form-group">
+              <input
+              style = {styles.loginFormTextInput}
+              value = {this.state.email}
+              onChange = {this.handleChange}
+              type = "email"
+              name = "email"
+              class = "form-control"
+              id = "exampleInputEmail1"
+              aria-describedby="emailHelp"
+              placeholder="username"
+              />
+            </div>
+      
+            <div>
+              <input
+              style={styles.loginFormTextInput}
+              value={this.state.password}
+              onChange={this.handleChange}
+              type="password"
+              name="password"
+              class="form-control"
+              id="exampleInputPassword1"
+              placeholder="Password"
+              />
+            </div>
+            
+            <div style={{textAlign:"center"}}>
+              <button
+              style={styles.loginButton}
+              type="submit"
+              onClick={
+                (e) => {
+                  this.login(e);
+                }
+              }
+              class="btn btn-primary"
+              >
+                Login
+              </button>
+            </div>
+            
+          </Container>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Login;
--- src/containers/profile.js
+++ src/containers/profile.js
@@ -15,96 +15,132 @@ import Menu from '@material-ui/core/Menu';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 const styles = (theme) => ({
+	
   root: {
+	  
     margin: 0,
     padding: theme.spacing(2),
+	
   },
   closeButton: {
+	  
     position: 'absolute',
     right: theme.spacing(1),
     top: theme.spacing(1),
     color: theme.palette.grey[500],
+	
   },
+  
 });
 
 const DialogTitle = withStyles(styles)((props) => {
+	
   const { children, classes, onClose, ...other } = props;
+  
   return (
+  
     <MuiDialogTitle disableTypography className={classes.root} {...other}>
       <Typography variant="h6">{children}</Typography>
       {onClose ? (
+	  
         <IconButton aria-label="close" className={classes.closeButton} onClick={onClose}>
           <CloseIcon />
         </IconButton>
+		
       ) : null}
     </MuiDialogTitle>
+	
   );
+  
 });
 
 const DialogContent = withStyles((theme) => ({
+	
   root: {
+	  
     padding: theme.spacing(2),
+	
   },
+  
 }))(MuiDialogContent);
 
 const DialogActions = withStyles((theme) => ({
+	
   root: {
+	  
     margin: 0,
     padding: theme.spacing(1),
+	
   },
+  
 }))(MuiDialogActions);
 
 export default function Profile() {
+	
   const [open, setOpen] = React.useState(false);
 
   const handleClickOpen = () => {
+	  
     setOpen(true);
+	
   };
   const handleClose = () => {
+	  
     setOpen(false);
+	
   };
 
   return (
+  
     <div>
+	
       <MenuItem onClick={handleClickOpen}>
         <Typography variant = "h5">
           Profile
         </Typography>
       </ MenuItem>
+	  
       <Dialog onClose={handleClose} aria-labelledby="customized-dialog-title" open={open} >
         <DialogTitle id="customized-dialog-title" onClose={handleClose}>
           <Typography variant = "h4">
             Profile
           </Typography>
         </DialogTitle>
+		
         <DialogContent dividers>
             <Avatar alt="Arcata" src={ResImage} style={{width:60, height:60}}/>
-            <Card>
-              <CardContent>
-              <Typography variant="h5">
-        <b>Email Address:</b>  sample@email.com
-        <br />
-        <br />
-        <b>Name of restaurant:</b>  Arcata Pizzeria
-      </Typography>
-              </CardContent>
-            </Card>
+			  <Typography variant = "h5"> <br /> </Typography>
+              <Card>
+                <CardContent>
+                  <Typography variant="h5">
+                    <b>Email Address:</b>  sample@email.com
+                    <br />
+                    <br />
+                    <b>Name of restaurant:</b>  Arcata Pizzeria
+                  </Typography>
+                </CardContent>
+              </Card>
+			  
               <MenuItem>
                 <Typography variant = "h5">
                   Settings
                 </Typography>
               </MenuItem>
+			  
               <MenuItem>
                 <Typography variant = "h5">
                   Forgot password
                 </Typography>
               </MenuItem>
+			  
               <MenuItem>
                 <Typography variant = "h5">
                   Help
                 </Typography>
               </MenuItem>
+			  
         </DialogContent>
+		
         <DialogActions>
           <Button autoFocus onClick={handleClose} color="primary">
             <Typography variant = "h5">
@@ -114,5 +150,7 @@ export default function Profile() {
         </DialogActions>
       </Dialog>
     </div>
+	
   );
+  
 }
