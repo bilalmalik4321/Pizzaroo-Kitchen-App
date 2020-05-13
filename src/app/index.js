752142b41a0794a55f19545762aafbf45c60e383
Prakort <leanprakort@gmail.com>
2020-05-13 13:00:58 -0400

    move routes into app.js

--- src/app/index.js
+++ src/app/index.js
@@ -1,9 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "typeface-roboto";
-
-import { subscribe } from 'react-contextual';
-import firebase from 'firebase';
-
 import { useRoutes } from 'hookrouter';
 import NotFound from '../app/layout/NotFound';
 import Home from "../account/homePage";
@@ -14,26 +10,8 @@ import Auth from './auth';
 import Protected from './protected';
 
 
-const App = (props) => {
+const App = () => {
 
-  const [loggedIn , setLoggedIn] = useState(false);
-  const [loading, setLoading] = useState(true);
-  // console.log("current user", userInfo);
-
-  // useEffect(() => {
-  //     firebase.auth().onAuthStateChanged(async user => {
-  //       if (user) {
-  //         setLoggedIn(true);
-  //         setLoading(false);
-  //       } else {
-  //         setLoggedIn(false);
-  //         setLoading(false);
-  //       }
-  //     });
-  // }, [loggedIn]);
-
-
-  console.log("app--set loggedin", loggedIn);
   const routes = {
     '/': ()=> <Home/>,
     '/login': ()=> <Login/>,
@@ -43,16 +21,9 @@ const App = (props) => {
   
   const Routes = useRoutes(routes);
 
-  const path = window.location.pathname;
-
-  console.log("App -- find", Routes);
-  // if(loading) {
-  //   return <></>
-  // }
-
   if(!Routes)
     return <NotFound/>
 
-  return <Auth isLoggedIn={loggedIn}>{Routes}</Auth> 
+  return <Auth>{Routes}</Auth> 
 }
-export default subscribe()(App);
+export default App;
